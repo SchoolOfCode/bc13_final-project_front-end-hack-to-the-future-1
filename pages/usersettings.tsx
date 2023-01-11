c2c9cc0b71bd3878dac6c18f4daa7c9bc738c09d
Mrbusy <nickhughes13@googlemail.com>
2023-01-11 15:16:17 +0000

    button added to usersettings page

--- pages/usersettings.tsx
+++ pages/usersettings.tsx
@@ -1,10 +1,18 @@
 import { useSession } from "@supabase/auth-helpers-react";
 import { useProfile } from "../hooks/useProfile";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import Button from "../components/Button/Button";
 
 export default function UserSettings() {
   const { profile } = useProfile();
+
+  const supabase = useSupabaseClient() 
+  const handleClick=async () => {
+    await supabase.auth.signOut()
+  }
+
   return (
-    
+   
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {!profile ? (
         <p>not logged in</p>
@@ -13,6 +21,7 @@ export default function UserSettings() {
           <p>
             Hello {profile.full_name}. You are a {profile.user_type}
           </p>
+          <Button onClick={handleClick} buttonText="Logout"/>
         </div>
       )}
     </div>
