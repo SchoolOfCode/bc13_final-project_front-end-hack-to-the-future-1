70ef2a1b707e5d208dabaee64e49be66642e4d7a
OmertaDevs <omertar6@outlook.com>
2023-01-11 11:30:59 +0000

    added auth providers

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,12 +9,13 @@ export default function Login() {
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {!session ? (
         <Auth
+        providers={["facebook","google"]}
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
           theme="dark"
         />
       ) : (
-        <p>Account page will go here.</p>
+        <p>You are logged in</p>
       )}
     </div>
   );
