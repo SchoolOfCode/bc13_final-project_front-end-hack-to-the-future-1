18bca16a1f73f71c62895f155b52d3161d93a1bf
november-golf <n.s.gray@live.co.uk>
2023-01-11 14:44:24 +0000

    Map Objective complete: Render multiple locations from a fetch request to Supabase backend. All locations from Businesses table now render on screen.

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,35 +1,64 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import { useState, useEffect } from "react";
 import { supabase } from "../../supabase";
+import { Database } from "../../types/supabase";
+
+{/* REF:LOCATIONINTERFACEv1. This code describes the interface for Location which matches the data types of the 'Businesses' table in Supabase database. */}
+
+export interface Location { 
+  business_type: string
+  lat: number,
+  lon: number,
+  name: string
+} 
+
+{/* REF:MAPv2. This code makes a fetch request to Supabase database. It returns all data from the 'Businesses' table. The response is mapped and each item is stored in the 'locations' state. */}
 
 export default function Map() {
+  
+  //Add a state to contain the latitude and logitude of each business
+  const [locations, setLocations] = useState<Location[]>([]);
 
-  const [lat, setLat] = useState(null)
-  const [lon, setLon] = useState(null)
+  useEffect(() => {
+    async function getLocations() {
+      const { data } = await supabase
+        .from('businesses')
+        .select();
 
-// Struggling to type data and error on line 13.
-// This function is a fetch request to retrive sample data from businesses table.
+      const locationsData: any = data ? data.map((item) => ({ lat: item.lat, lon: item.lon, name: item.name, business_type: item.business_type })) : console.log("No data found");
+      setLocations(locationsData);
+      console.log(typeof(locationsData))
+    }
+    getLocations()
+  }, []);
 
-useEffect( () => {
-  async function getLocation(){
-    const {data, error} = await supabase.from('businesses').select(`lat, lon`)
-    setLat(data[0].lat)
-    setLon(data[0].lon)
+  
 
-  }
- 
-  type LocationResponse = Awaited<ReturnType<typeof getLocation>>
-  type LocationResponseSuccess = LocationResponse['data']
-  type LocationResponseError = LocationResponse['error']
-  getLocation()
-}, []);
+  
+{/* REF:MAPv1. This code makes a fetch request to the Supabase database. It returns all data from the 'Businesses' table. It stores lat and lon from a single row (data[0].lat & data[0].lon) in two pieces of state ('latitude' & 'longitude'). This code is superceded by REF:MAPv2 */}
 
+ //const [latitude, setLatitude] = useState<any | null>(["empty"]);
+ //const [longitude, setLongitude] = useState<any | null>(["empty"]);
 
-
+  // useEffect(() => {
+  //   async function getLocation() {
+  //     const { data, error } = await supabase
+  //       .from('businesses')
+  //       .select();
+  //       console.log(data, data[0].lat, data[0].lon)
+  //       setLatitude(data);
+  //       setLongitude(data);    
+  //     }
+  //   type LocationResponse = Awaited<ReturnType<typeof getLocation>>;
+  //   //type LocationResponseSuccess = LocationResponse['data'];
+  //   //type LocationResponseError = LocationResponse['error'];
+  //   getLocation();
+  // }, []);
+  
 
   return (
     <MapContainer
-      className="h-screen w-screen z-1"
+      className="h-screen w-screen"
       center={[53.367513, -1.501612]}
       zoom={13}
       scrollWheelZoom={true}
@@ -38,11 +67,26 @@ useEffect( () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[53.367513, -1.501612]}>
+
+{/* REF:MARKERv2. This code is rendering a list of markers on a map, where each marker corresponds to a location in the locations array which is stored as state. */}
+      <div>
+      {locations.map((location, i) => (
+        <Marker key={i} position={[location.lat, location.lon]}>
+          <Popup>
+            {location.name} <br /> {location.business_type}
+          </Popup>
+        </Marker>
+      ))}
+    </div>
+
+{/* REF: MARKERv1.This code is the first iteration. It renders a single marker on a map. The lat and lon values correspond with the response object of a fetch request that returned a single row (data[0].lat & data[0].lon – which were both stored as states: latitude & longitude). This has been superceded by code REF:MARKERv2 */}
+
+      {/* <Marker position={[lat, lon]}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          Popup info <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
+
     </MapContainer>
   );
 }
