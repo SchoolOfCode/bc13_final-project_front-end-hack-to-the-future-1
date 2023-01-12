1127e6b9e6022a5c9cbe566b428df7c87aeb02c1
Andrew <andrew@eseeds.com>
2023-01-12 16:07:56 +0000

    Deal cards data pulled successfully from database. Formatting text on cards being a right pain. Cards need a max width, text needs to be wrapped correctly

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../../supabase';
 
-const deals: string[] = [
-  "Free Muffin",
-  "50% off fresh loaves",
-  "£5 Coffee and cake",
-];
+//Deals constant below is hard-coded data used during development
+
+// const deals: string[] = [
+//   "Free Muffin",
+//   "50% off fresh loaves",
+//   "£5 Coffee and cake",
+// ];
+
+//Change from hard-coded deals to data fetched from the database
+
+export interface Deals {
+  name: string;
+  expiration_time: Date;
+  business_id: string;
+}
 
 export default function Carousel() {
+  const [offers, setOffers] = useState<Deals[]>([]);
+
+  useEffect(() => {
+    async function getDeals() {
+      const { data } = await supabase.from('deals').select();
+
+      const dealsData: any = data
+        ? data.map((item) => ({
+            name: item.name,
+            business_id: item.business_id,
+            expiration_time: item.expiration_time,
+          }))
+        : console.log('No data found');
+      setOffers(dealsData);
+    }
+    getDeals();
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center z-10">
-      
-        <ul className="flex flex-row px-5 gap-5 overflow-y-auto z-10">
-          {deals.map((deal, i) => (
-            <li
-              key={i}
-              className="flex-none w-10/12 py-10 bg-slate-700 rounded-3xl text-white text-center"
-            >
-              <h1 className="flex-none text-center text-2xl font-bold my-12 z-10">
-                Deal card
-              </h1>
-              {deal}
-            </li>
-          ))}
-        </ul>
-      
+    <div className='flex flex-col justify-center z-10 '>
+      <ul className='flex flex-row px-5 gap-5 overflow-y-auto z-10 '>
+        {offers.map((offer, i) => (
+          <li
+            key={i}
+            className='flex-none max-w-10/12 py-10 bg-slate-700 rounded-3xl text-white text-center'
+          >
+            <h1 className='flex-none text-center max-w-sm text-2xl font-bold my-12 z-10 '>
+              {offer.business_id}
+            </h1>
+            {offer.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
