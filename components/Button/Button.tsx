79a5a2fe54540e0665d32fefa68f51313c702885
OmertaDevs <omertar6@outlook.com>
2023-01-12 11:02:32 +0000

    styled button component as per hifi wireframe installed open sans as a global font

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 
 export default function Button({ buttonText, onClick }: ButtonProps) {
   return (
-    <button onClick={onClick} className="border-2">
+    <button onClick={onClick} className="border-2 bg-red-400 box-border h-10 w-32 border-slate-600 text-slate-600 rounded-md shadow-md shadow-slate-300 font-Open-semibold z-10">
       {buttonText}
     </button>
   );
