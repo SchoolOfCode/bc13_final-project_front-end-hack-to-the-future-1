e2d182fb50a073dd9902a385a216c67ee4b326e3
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-01-12 14:03:57 +0000

    conditional render of settings/login button on map page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,47 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
-import Map from '../components/Map/';
-
-const inter = Inter({ subsets: ['latin'] });
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Map from "../components/Map/";
+import Button from "../components/Button/Button";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const user = useUser();
+  const router = useRouter();
+  function redirectToSettings() {
+    router.push("/usersettings");
+  }
+  function redirectToLogIn() {
+    router.push("/login");
+  }
   return (
     <>
       <Head>
         <title>IndyGo</title>
-        <meta name='description' content='Support local businesses!' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Support local businesses!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
         <link
-          rel='stylesheet'
-          href='https://unpkg.com/leaflet@1.9.3/dist/leaflet.css'
-          integrity='sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI='
-          crossOrigin=''
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
         />
       </Head>
-      <header>
-        <h1 className='z-10 absolute top-0 right-0'>IndyGo</h1>
+      <header className="flex justify-between w-full z-10 absolute top-0 border-box p-4">
+        <h1>IndyGo</h1>
+        {user ? (
+          <Button buttonText="Settings" onClick={redirectToSettings} />
+        ) : (
+          <Button buttonText="Log In" onClick={redirectToLogIn} />
+        )}
       </header>
+
       <main>
-        <div className='z-0'>
+        <div className="z-0">
           <Map />
         </div>
       </main>
