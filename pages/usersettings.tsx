61622052a9ecec0be1e541699066ff81155aa210
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-01-16 11:28:12 +0000

    support for updating user email, name and password through supabase

--- pages/usersettings.tsx
+++ pages/usersettings.tsx
@@ -1,25 +1,60 @@
-import { useSession } from "@supabase/auth-helpers-react";
 import { useProfile } from "../hooks/useProfile";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import Button from "../components/Button/Button";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function UserSettings() {
   const { profile } = useProfile();
+  const user = useUser();
   const router = useRouter();
-
   const supabase = useSupabaseClient();
+
+  const [name, setName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  useEffect(() => {
+    if (profile) {
+      setName(profile.full_name);
+    }
+    if (user) {
+      setEmail(user.email);
+    }
+  }, [user, profile]);
+
+  const saveChanges = async () => {
+    if (user) {
+      const { data, error } = await supabase.auth.updateUser({
+        email: email,
+      });
+    }
+
+    if (user) {
+      const { error } = await supabase
+        .from("profiles")
+        .update({ full_name: name })
+        .eq("id", user.id);
+      if (error) {
+        throw error;
+      }
+    }
+
+    if (password) {
+      const { data, error } = await supabase.auth.updateUser({
+        password: password,
+      });
+    }
+  };
+
   const handleClick = async () => {
     await supabase.auth.signOut();
     router.push("/");
   };
+
   const redirectToRoot = () => {
     router.push("/");
-  };
-  const saveChanges = async () => {
-    //not sure on the logic of how to do this? is it a state based thing with handle submit?
   };
 
   return (
@@ -33,35 +68,56 @@ export default function UserSettings() {
             <Button onClick={handleClick} buttonText="Logout" />
           </header>
           <div className="flex flex-col justify-center gap-4 items-center text-center h-5/6 w-5/6 max-w-md bg-slate-400 py-10">
-            <h1 className="font-Open-semi-bold text-xl text-slate-800">
-              Hello {profile.full_name}
+            <h1 className="font-Open-semi-bold text-md text-slate-800">
+              Edit Account Details
             </h1>
-            <h2 className="font-Open-semi-bold text-md text-slate-800">
-              {" "}
-              Edit account details
-            </h2>
-            <form className= "flex flex-col justify-center gap-4 items-center text-center h-5/6 w-5/6 max-w-md bg-slate-400 py-10">
-              <label
-                htmlFor="email"
-                className="font-Open text-sm text-slate-800"
-              >
-                Account Email
-              </label>
-              <input id="email" name="email" />
-              <label
-                htmlFor="password"
-                className="font-Open text-sm text-slate-800"
-              >
-                Password
-              </label>
-              <input id="password" name="password" />
+            <p>{name}</p>
+            <p>{email}</p>
+            <form className="flex flex-col justify-center gap-4 items-center text-center h-5/6 w-5/6 max-w-md bg-slate-400 py-10">
               <label
                 htmlFor="name"
                 className="font-Open text-sm text-slate-800"
               >
                 Full Name
               </label>
-              <input id="name" name="name" />
+              <input
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+
+              <label
+                htmlFor="email"
+                className="font-Open text-sm text-slate-800"
+              >
+                Email Address
+              </label>
+              <input
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+
+              <label
+                htmlFor="password"
+                className="font-Open text-sm text-slate-800"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
             </form>
             <Button onClick={saveChanges} buttonText="Save Changes" />
             <Button onClick={redirectToRoot} buttonText="Back" />
