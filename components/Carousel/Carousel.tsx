56ab7f56b9d8866bd5d928883ab7ae83cd330578
Andrew <andrew@eseeds.com>
2023-01-16 17:24:40 +0000

    Formatted

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { supabase } from "../../supabase";
-import {GiSombrero} from "react-icons/gi"
-import {RiCake3Line} from "react-icons/ri"
-import {RiRestaurantFill} from "react-icons/ri"
-import { isTemplateExpression } from "typescript";
-
-
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../../supabase';
+import { GiSombrero } from 'react-icons/gi';
+import { RiCake3Line } from 'react-icons/ri';
+import { RiRestaurantFill } from 'react-icons/ri';
+import { isTemplateExpression } from 'typescript';
 
 //Deals constant below is hard-coded data used during development
 
@@ -24,70 +22,67 @@ export interface Deals {
   business_name: string;
 }
 
-export default function Carousel({businessData}:any) {
-
-//   Storing the props object in a variable.
-
-
-  
+export default function Carousel({ businessData }: any) {
+  //   Storing the props object in a variable.
 
   //Map through offers. Compare offers.business_id with businessData.business_id. If match replace offers.business_name with businessData.name. Update a mergedInfo state with the new array.
   const [offers, setOffers] = useState<Deals[]>([]);
 
-
-  let businessStuff = offers
-  console.log(businessStuff)
+  let businessStuff = offers;
+  console.log(businessStuff);
 
   useEffect(() => {
-
-    let updatedOffers = []
+    let updatedOffers: any = [];
     offers.forEach((item1) => {
-        businessData.forEach((item2)=> {
-            if (item1.business_id === item2.business_id){
-                item1.business_name = item2.name
-                updatedOffers.push(item1)
-            }
-        })
-    })
-    setOffers(updatedOffers)
-    
-    }, [businessData]);
+      businessData.forEach((item2: any) => {
+        if (item1.business_id === item2.business_id) {
+          item1.business_name = item2.name;
+          updatedOffers.push(item1);
+        }
+      });
+    });
+    setOffers(updatedOffers);
+  }, [businessData]);
 
- 
-
-
-
- 
-
-
-  
-    useEffect(() => {
+  useEffect(() => {
     async function getDeals() {
-      const { data } = await supabase.from("deals").select();
+      const { data } = await supabase.from('deals').select();
 
       const dealsData: any = data
         ? data.map((item) => ({
             name: item.name,
             business_id: item.business_id,
             expiration_time: item.expiration_time,
-            business_name: null
+            business_name: null,
           }))
-        : console.log("No data found");
+        : console.log('No data found');
       setOffers(dealsData);
     }
     getDeals();
   }, []);
 
   return (
-    <div className="flex flex-col justify-center z-10 w-screen">
-      <ul id="deal-carousel" className="flex flex-row px-5 gap-5 overflow-y-auto z-10 w-screen">
+    <div className='flex flex-col justify-center z-10 w-screen'>
+      <ul
+        id='deal-carousel'
+        className='flex flex-row px-5 gap-5 overflow-y-auto z-10 w-screen'
+      >
         {offers.map((offer, i) => (
           <li
             key={i}
-            id="deal-card" className="flex-none max-w-10/12 py-10 bg-slate-700 rounded-3xl text-white text-center max-w-md max-h-md"
+            id='deal-card'
+            className='flex-none max-w-10/12 py-10 bg-slate-700 rounded-3xl text-white text-center max-w-md max-h-md'
           >
-            <div id="b-type-icon" className="flex justify-center flex-wrap max-w-sm max-h-sm z-10"><RiRestaurantFill size='2rem'/></div>
-            <h1 id="business-name" className="flex justify-items-center text-center flex-wrap max-w-sm text-2xl font-bold my-12 z-10 ">
+            <div
+              id='b-type-icon'
+              className='flex justify-center flex-wrap max-w-sm max-h-sm z-10'
+            >
+              <RiRestaurantFill size='2rem' />
+            </div>
+            <h1
+              id='business-name'
+              className='flex justify-items-center text-center flex-wrap max-w-sm text-2xl font-bold my-12 z-10 '
+            >
               {offer.business_name}
             </h1>
             {offer.name}
@@ -97,4 +92,3 @@ export default function Carousel({businessData}:any) {
     </div>
   );
 }
-
