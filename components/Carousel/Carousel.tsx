582ebe84c9242817b94f46655b5140a5f9b56f87
OmertaDevs <omertar6@outlook.com>
2023-01-23 15:47:31 +0000

    hooked up deal card to the new state that stores the offers but difficult to test on my end so pushing to nick who can check it

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../../supabase';
-import { GiSombrero } from 'react-icons/gi';
-import { RiCake3Line } from 'react-icons/ri';
-import { RiRestaurantFill } from 'react-icons/ri';
+
 import { isTemplateExpression } from 'typescript';
 import DealCard from '../DealCard/DealCard';
 import { useLocation } from '../../hooks/useLocation';
@@ -10,23 +8,29 @@ import { PostcodesFetch } from '../../types/fetch';
 
 export interface Deals {
   id: string;
-  name: string;
   expiration_time: string;
   business_id: string;
   business_name: string;
+  address_id: string | null;
+  business_type: string | null;
+  created_at: string | null;
+  lat: number | null;
+  lon: number | null;
+  name: string 
+  website: string | null;
 }
 
 export default function Carousel({ businessData }: any) {
   const [offers, setOffers] = useState<Deals[]>([]);
   //todo type will be what ever is returned from fetch
-  const [postcodes, setPostcodes] = useState<PostcodesFetch>([]);
+  const [postcodes, setPostcodes] = useState<PostcodesFetch[]>([]);
   // long and lat from geolocation (useLocation())
   const { pos } = useLocation();
   const mappedPostcodes: any = [];
 
   useEffect(() => {
     if (pos) {
-      const getAllLocalDeals = async () => {
+      const getAllLocalPostcodes = async () => {
         const response = await fetch(
           `https://api.postcodes.io/postcodes?lon=${pos.lng}&lat=${pos.lat}&radius=1000`
         );
@@ -42,19 +46,21 @@ export default function Carousel({ businessData }: any) {
                
         // setOffers(deals);
       };
-      getAllLocalDeals();
+      getAllLocalPostcodes();
     }
   }, [pos]);
   console.log('Postcodes state', postcodes);
 
   useEffect(() => {
-    const localDeals= async()=>{
+    const getAllLocalDeals= async()=>{
   const { data } = await supabase
   .from("businesses")
   .select("*, deals (id,name,expiration_time, created_at)")
   .in("postcode", [postcodes]);
-console.log('deals', data, postcodes)}
-localDeals()
+      console.log('deals', data, postcodes)
+      setOffers(data)
+    }
+getAllLocalDeals()
   }, [postcodes]);
 
   // save the fetch object as variable (localPostcodes)
@@ -66,27 +72,27 @@ localDeals()
   //? What is the dependency?
   //! DO NOT let this send infinite requests!!
 
-  useEffect(() => {
-    async function getDeals() {
-      const { data } = await supabase
-        .from('deals')
-        .select('*, businesses (name)');
+  // useEffect(() => {
+  //   async function getDeals() {
+  //     const { data } = await supabase
+  //       .from('deals')
+  //       .select('*, businesses (name)');
      
-      const dealsData: any = data
-        ? data.map((item) => ({
-            id: item.id,
-            name: item.name,
-            business_id: item.business_id,
-            expiration_time: item.expiration_time,
-            business_name: Array.isArray(item.businesses)
-              ? item.businesses[0].name
-              : item.businesses?.name,
-          }))
-        : console.log('No data found');
-      setOffers(dealsData);
-    }
-    getDeals();
-  }, []);
+  //     const dealsData: any = data
+  //       ? data.map((item) => ({
+  //           id: item.id,
+  //           name: item.name,
+  //           business_id: item.business_id,
+  //           expiration_time: item.expiration_time,
+  //           business_name: Array.isArray(item.businesses)
+  //             ? item.businesses[0].name
+  //             : item.businesses?.name,
+  //         }))
+  //       : console.log('No data found');
+  //     setOffers(dealsData);
+  //   }
+  //   getDeals();
+  // }, []);
 
   function getTimeRemaining(offerExpiry: string) {
     let expiration_string = '';
@@ -121,7 +127,7 @@ localDeals()
         {offers.map((offer) => (
           <DealCard
             key={offer.id}
-            businessName={offer.business_name}
+            businessName={offer.name}
             dealText={offer.name}
             dealHighlight={getTimeRemaining(offer.expiration_time)}
           />
