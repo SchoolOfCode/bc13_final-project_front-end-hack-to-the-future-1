7de3b68afb3792c30651d31a204fc4b3d27437fc
november-golf <n.s.gray@live.co.uk>
2023-01-23 14:26:29 +0000

    E2E Testing suite setup, and two test specs written: |Signed up user, creates a business account| New user signs up, request sent to Supabase, a message is displayed. Button & UserType  components modified to contain data-cy tags for Cypress testing.

---
+++ cypress/e2e/specBusinessAccountSetup.cy.ts
@@ -0,0 +1,23 @@
+  describe('Signed up user, creates a business account', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('create new transaction', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('.border-2').click();
+    cy.wait(4000)
+    cy.get('.c-cpTgHx-bBzSYw-type-default').type('bewag41727@fom8.com');
+    cy.get('.c-cpTgHx-kowJZS-type-password').type('password123');
+    cy.get('#auth-sign-in > div > button').click();
+    cy.wait(4000)
+    cy.visit('http://localhost:3000/usertype')
+    cy.get('[data-cy="name-field"]').type('Steven Cypress');
+    cy.wait(4000)
+    cy.get('[data-cy="CONTINUE AS BUSINESS"]').click();
+    
+    
+
+  })
+
+});
